03c4e92d378e975937019b2e06efef6524e15557
ramyaravikumar22 <ramya38br@gmail.com>
2022-08-15 19:30:04 -0500

    saving progress

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let usersWord = "" //this is defined globally. let is allowing us to change in the course of the program
+let usersWord = "" 
 let oddList = document.getElementById("odd")
 let evenList = document.getElementById("even")
 
@@ -6,7 +6,6 @@ let evenList = document.getElementById("even")
     function updateWord(element) {
       usersWord = element.value
 
-     // console.log(usersWord)
     }
 
     function handleSubmit() {
@@ -20,18 +19,7 @@ let evenList = document.getElementById("even")
             document.getElementById("odd").appendChild(newListItem)
         }
 
-/*
-            let newListItem = document.createElement('LI')
-            newListItem.innerText = usersWord
-            oddList.appendChild(newListItem)
-        } else{
-            let newListItem = document.createElement('LI')
-            newListItem.innerText = usersWord
 
-            evenlist.appendChild(newListItem)
-        }*/
-    //we are appending a child and child is a newest item. its a built in function in java script and lists the items
-        
         usersWord = ""
         document.getElementById("even-odd-form").reset() //reset is a built in js. figure out a html element
       }
